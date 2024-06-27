107de0ca45fc9eb34da6070d08635957da929cab
Fernando <fernando.r100k@gmail.com>
2024-06-27 15:51:43 -0400

    feat: animação entrada nova

--- headline/app/page.tsx
+++ headline/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
     <div className="">
       <div className="relative h-screen flex overflow-hidden">
         <div className="absolute w-full items-center justify-center flex flex-col text-center text-white z-20 inset-0 ">
-          <h1 className="max-w-4xl text-xl md:text-3xl tracking-in-contract">Nós transformamos experts desconhecidos em verdadeiras celebridades - enquanto vendem milhões!</h1>
-          <p className="md:text-xl text-lg mt-2">Você pode ser o próximo…</p>
+          <h1 className="max-w-4xl text-xl md:text-3xl text-focus-in">Nós transformamos experts desconhecidos em verdadeiras celebridades - enquanto vendem milhões!</h1>
+          <p className="md:text-xl text-lg mt-2 text-focus-in-demorado">Você pode ser o próximo…</p>
           <Button nameButton={"Bora escalar?"} link={"https://wa.me/5567998128389"} />
         </div>
         <div className="-top-60 absolute inset-0">
