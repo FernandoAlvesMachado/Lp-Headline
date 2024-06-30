9348e30bd2974066facb6996b9d5177ca2f3aa56
Fernando <fernando.r100k@gmail.com>
2024-06-29 22:12:57 -0400

    feat: footer

--- headline/app/components/Footer.tsx
+++ headline/app/components/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
         <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                    <a href="#" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                         <Image src={LogoBrancaHorizontal} width={250} height={25} alt="logo branca" />
                     </a>
                     <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
