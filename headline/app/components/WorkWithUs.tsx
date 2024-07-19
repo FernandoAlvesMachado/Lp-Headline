fe4fc793195b04f4037f32d9f5765bdcc65b5be2
Fernando <fernando.r100k@gmail.com>
2024-07-18 23:10:48 -0400

    feat: new page aplication

--- headline/app/components/WorkWithUs.tsx
+++ headline/app/components/WorkWithUs.tsx
@@ -7,13 +7,11 @@ export default function WorkWithUs() {
                     Quero trabalhar com a gente?
                     </h1>
                     <div className="flex gap-12 flex-wrap md:flex-nowrap  w-full ">
-
-
                         <div className="w-full p-6  border border-gray-200 rounded-lg shadow bg-gray-800 border-gray-700">
                             <a href="#">
                                 <h5 className="mb-2 w-full  text-center md:text-start text-2xl font-bold tracking-tight text-gray-900 text-white">É expert e quer ser lançado por nós?</h5>
                             </a>
-                            <a href="#" className="inline-flex my-3 px-3 py-2  items-center w-full text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
+                            <a href="https://www.agenciaheadline.com.br/vagas-aplicacao" className="inline-flex my-3 px-3 py-2  items-center w-full text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
                                 CLIQUE AQUI
                                 <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
@@ -24,7 +22,7 @@ export default function WorkWithUs() {
                             <a href="#">
                                 <h5 className="w-full mb-2 text-center md:text-start text-2xl font-bold tracking-tight text-gray-900 text-white">Quer entrar para o nosso time?</h5>
                             </a>
-                            <a href="#" className="inline-flex my-3  px-3 py-2 items-center w-full  text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
+                            <a href="https://www.agenciaheadline.com.br/vagas-aplicacao" className="inline-flex my-3  px-3 py-2 items-center w-full  text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
                                 CLIQUE AQUI
                                 <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
---
+++ headline/app/vagas/page.tsx
@@ -0,0 +1,58 @@
+import Button from "../components/Button";
+import Carrossel from "../components/Carrossel";
+import PrimeiraDobra from "../components/firstPage";
+import Footer from "../components/Footer";
+import ScrollPage from "../components/ScrollSecction";
+import SecctionEspecialistas from "../components/SecctionEspecialistas";
+import StepsSecction from "../components/StepsSecction";
+import WhatWeDo from "../components/WhatWeDo";
+import WorkWithUs from "../components/WorkWithUs";
+
+
+export default function Vagas() {
+  return (
+    <div className=" overflow-x-hidden">
+      <div className="noise"></div>
+      <div className="relative h-screen flex overflow-hidden">
+        <div className="absolute w-full p-6 items-center justify-center mt-40 md:mt-72  flex flex-col text-center text-white z-50 inset-0 ">
+          {/* <h1 className="max-w-4xl text-2xl md:text-3xl text-focus-in">Nós transformamos experts desconhecidos em verdadeiras celebridades - enquanto vendem milhões!</h1>
+          <p className="md:text-xl text-lg mt-2 text-focus-in-demorado">Você pode ser o próximo…</p>
+          <Button nameButton={"Bora escalar?"} link={"#"} /> */}
+          <WorkWithUs />
+        </div>
+        <div className="-top-60 absolute inset-0">
+          <PrimeiraDobra />
+        </div>
+      </div>
+      <div className="relative ">
+        <ScrollPage />
+      </div>
+      <div className="relative my-10 flex flex-col items-center justify-center">
+        <h1 className="text-3xl text-center text-white">Aqui temos o time mais completo.</h1>
+        <Carrossel />
+      </div>
+      {/* <div className="relative w-full flex items-center justify-center">
+          <SegundaDobra />
+        </div> */}
+      <div>
+        <WhatWeDo />
+      </div>
+      <div className=" flex items-center justify-center">
+        <StepsSecction />
+      </div>
+      <div className=" flex items-center justify-center">
+        <SecctionEspecialistas />
+      </div>
+      <div className=" flex items-center justify-center">
+        <div className=" text-white items-center justify-center flex flex-col">
+          <h1 className="max-w-4xl text-2xl md:text-3xl text-focus-in text-center">Nós transformamos experts desconhecidos em verdadeiras celebridades - enquanto vendem milhões!</h1>
+          <p className="md:text-xl text-lg mt-2 text-focus-in-demorado">Você pode ser o próximo…</p>
+          <Button nameButton={"Bora escalar?"} link={"#"} />
+        </div>
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+}
